9e480132e174229b1a0263e8b63881ba29ee9d11
Renildo Ribeiro Agostini <renildoribeiroagostini@gmail.com>
2024-10-29 15:19:46 -0300

    Atualizando

--- Semana11/Desenvolvimento de um Catálogo de Produtos Interativo/script.js
+++ Semana11/Desenvolvimento de um Catálogo de Produtos Interativo/script.js
@@ -1,16 +1,15 @@
 //Declara o vetor global
-var produtos = [];
+let produtos = [];
 
 //Cria referência ao btAdicionar e associa a função ao evento click deste botão
-var btAdicionar = document.getElementById("btAdicionar");
-btAdicionar.addEventListener("click", adicionarProdutos);
+let btAdicionar = document.getElementById("btAdicionar").addEventListener("click", adicionarProdutos);
 
 
 function adicionarProdutos(){
     //Cria referência aos elementos contendo todos os dados de entrada
-    var inProduto = document.getElementById("inProduto");
-    var inDescricao = document.getElementById("inDescricao");
-    var inPreco = document.getElementById("inPreco");
+    let inProduto = document.getElementById("inProduto");
+    let inDescricao = document.getElementById("inDescricao");
+    let inPreco = document.getElementById("inPreco");
 
     //obtém conteúdo dos campos
     var produto = inProduto.value;
@@ -50,8 +49,9 @@ function adicionarProdutos(){
     //Percorre os elementos do vetor
     for (var i = 0; i < produtos.length; i++){
         //Adiciona a lista, cada objeto do vetor
-        lista += "Nome do Produto: " + produtos[i].produto + " Descrição: " + produtos[i].descricao  + " - R$: " + produtos[i].preco.toFixed(2) + "\n"
+                lista += `<span style="color: blue;">Nome do Produto:</span> ${produtos[i].produto} <span style="color: blue;">Descrição:</span> ${produtos[i].descricao} <span style="color: blue;"> Preço: </span>R$ ${produtos[i].preco.toFixed(2)} \n`;
+        
     }
 
-    document.getElementById("outLista").textContent = lista;
+    document.getElementById("outLista").innerHTML = lista;
 }
